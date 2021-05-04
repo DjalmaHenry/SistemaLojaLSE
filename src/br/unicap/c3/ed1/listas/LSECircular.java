9faff90c0e212dd46be9744d6b3210ca5af1e0ea
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-05-04 11:22:16 -0300

    add LSE Circular

---
+++ src/br/unicap/c3/ed1/listas/LSECircular.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unicap.c3.ed1.listas;
+
+/**
+ *
+ * @author djalm
+ */
+public class LSECircular {
+    
+}
